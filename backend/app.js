4c953e0339a6ac93aa7aaae7715fdb3ff8641c8f
Tunde <tundealabi4780@gmail.com>
2020-11-23 12:24:21 +0100

    Initialize loading html page

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,14 @@
 module.exports.add = (a,b) => a + b; 
-module.exports.multiply = (a,b) => a * b; 
+module.exports.multiply = (a,b) => a * b;
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",`${path.join(__dirname,'views')}`)
+
+app.get("/",(req,res,next)=>{
+    return res.render("index")
+})
+app.listen(3900,()=>console.log("Server is hot @ 3900"));
 
