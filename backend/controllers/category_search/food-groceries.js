47e2542768849de24b500f2c296bdc465fbf4639
Tunde <tundealabi4780@gmail.com>
2020-12-20 13:25:33 +0100

    add cookie 2

--- backend/controllers/category_search/food-groceries.js
+++ backend/controllers/category_search/food-groceries.js
@@ -19,7 +19,7 @@ const jumia = async () =>{
         // let $ = cheerio.load(response.data);
         request('https://www.jumia.com.ng/groceries/',{
           headers: {
-            cookie: 'newsletter=1; userLanguage=en_NG; __cfduid=de7b3715e97762d281ff0cd494c316b521607525202'
+            cookie: 'newsletter=1; userLanguage=en_NG; _gcl_au=1.1.1723443389.1604724214; _ga=GA1.3.1316592985.1604724214; _fbp=fb.2.1604724215481.197052287; _cs_ex=1521463277; _cs_c=1; sponsoredUserId=42241284650743594515fa9118f790d5; gadsTest=test; sb-closed=true; subscriberId=7ca271dc93d3b109c035f130d9534b90; accountType=Customer; customerType=new; userId=10276433; customerUuid=48a3b733-96d3-4f72-ba40-b165fa83fd20; __cfduid=de7b3715e97762d281ff0cd494c316b521607525202; closedBanners=63; __gads=ID=4de9c891817b93a7:T=1604915603:S=ALNI_MYzAvyTZmvjbmun2253UZZ_G42Gtg; _gid=GA1.3.651008294.1608427510; SOLSESSID=067d62dffefa88533aafff6d9c5639e9; cto_bundle=GaBjwV9pTU9Fc01CUlczNjhmZWFLRmpKcjM1SkFJQ2tVZndXT1JlcThZODZCOGc3ZGlTbWMlMkJmeHdlcHZSQ0FpNVBEU1k1eHgyU0wxUSUyQnZTeUlxV0JvbkVwZkRxMElERXh5R284OW5iUHJITnZLRk9UWFJIQWcyVjk2WjI5b1RrNGExZTNHZGtKSllpTzBuaiUyRjQxT29aMThjT0ElM0QlM0Q; _gat_UA-31880622-1=1'
           }
         }, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
