11358a9de7a8c7b81f1daf516ecef693432315e9
thebadmos <badmosolamiji@gmail.com>
2020-12-24 05:36:45 +0100

    phases added

---
+++ assets/modal.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.getVendor').click(function(){
+		$('.modal-ven').show();
+		
+	});
+	$('.modal-ven .close').click(function(){
+		$('.modal-ven').hide();
+		
+	})
+
+
+})
--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,30 @@
 
 document.querySelector('.navbarToggle2').addEventListener('click', classToggle);
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function openModalBox(){
+    var modal = $('.modal, .mask');
+  $('.open-modal').on('click', function() {
+   modal.fadeIn(300);
+  });
+  $('.close-modal, .mask').on('click', function() {
+   modal.fadeOut(800);
+  });
+  }
+  openModalBox();
