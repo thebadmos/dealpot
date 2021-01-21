ebb127ce66f2a1a1f9c985cab4d210706163f615
Tunde <tundealabi4780@gmail.com>
2021-01-21 03:20:36 +0100

    frontend and backend merge complete v1.2-fix search button

--- backend/public/js/index.js
+++ backend/public/js/index.js
@@ -1,11 +1,16 @@
 const form = document.getElementById("search-form");
 const input = document.getElementById("search-box");
+const searchBtn = document.getElementById("search-btn");
 const currentUrl = location.origin;
 
 document.addEventListener("DOMContentLoaded",()=>{
+    searchBtn.addEventListener("click",(e)=>{
+        if(input.value.length){
+            location.assign(`${currentUrl}/search/?q=${input.value}`);
+        }
+    });
     form.addEventListener("submit",(e)=>{
         e.preventDefault();
-        console.log(`${currentUrl}/search/?q=${input.value}`);
         location.assign(`${currentUrl}/search/?q=${input.value}`);
     });
 });
